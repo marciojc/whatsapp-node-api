bd31231303decb3eafa02bb80fad713f973fda95
Márcio Coelho <468890+marciojc@users.noreply.github.com>
2022-04-11 22:50:05 +0100

    Update auth component

--- components/auth.js
+++ components/auth.js
@@ -31,26 +31,26 @@ function sendQr(res) {
   fs.readFile('components/last.qr', (err, last_qr) => {
     if (!err && last_qr) {
       var page = `
-                    <html>
-                        <body>
-                            <script type="module">
-                            </script>
-                            <div id="qrcode"></div>
-                            <script type="module">
-                                import QrCreator from "https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js";
-                                let container = document.getElementById("qrcode");
-                                QrCreator.render({
-                                    text: "${last_qr}",
-                                    radius: 0.5, // 0.0 to 0.5
-                                    ecLevel: "H", // L, M, Q, H
-                                    fill: "#536DFE", // foreground color
-                                    background: null, // color or null for transparent
-                                    size: 256, // in pixels
-                                }, container);
-                            </script>
-                        </body>
-                    </html>
-                `;
+        <html>
+          <body>
+            <script type="module">
+            </script>
+            <div id="qrcode"></div>
+            <script type="module">
+              import QrCreator from "https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js";
+              const container = document.getElementById("qrcode");
+              QrCreator.render({
+                text: "${last_qr}",
+                radius: 0.5, // 0.0 to 0.5
+                ecLevel: "H", // L, M, Q, H
+                fill: "#000000", // foreground color
+                background: null, // color or null for transparent
+                size: 256, // in pixels
+              }, container);
+            </script>
+          </body>
+        </html>
+      `;
       res.write(page);
       res.end();
     }
